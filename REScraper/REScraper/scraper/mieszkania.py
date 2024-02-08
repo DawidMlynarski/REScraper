caf11e897fe128b90d4a4891f374aa49f6fcfc77
Dawid Mlynarski <dwdmlynarski@gmail.com>
2024-02-09 00:00:02 +0100

    minor bug fixes

--- REScraper/REScraper/scraper/mieszkania.py
+++ REScraper/REScraper/scraper/mieszkania.py
@@ -4,8 +4,8 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
 # Function to scrape a specific site
-def scrape_page(page_number,location,transaction,number_of_rooms):
-    url = f"https://www.olx.pl/nieruchomosci/mieszkania/{transaction}/{location}/?page={page_number}/&search%5Bfilter_enum_rooms%5D%5B0%5D={number_of_rooms}"
+def scrape_page(page_number,location,transaction,number_of_rooms,sorting):
+    url = f"https://www.olx.pl/nieruchomosci/mieszkania/{transaction}/{location}/?page={page_number}&search%5Bfilter_enum_rooms%5D%5B0%5D={number_of_rooms}&search%5Border%5D=created_at:desc"
     page_to_scrape = requests.get(url)
     soup = BeautifulSoup(page_to_scrape.text, "html.parser")
     titles = soup.find_all("h6", attrs={"class":"css-16v5mdi er34gjf0"})
@@ -13,7 +13,7 @@ def scrape_page(page_number,location,transaction,number_of_rooms):
     location_of_offer = soup.find_all("p", attrs={"data-testid":"location-date","class":"css-1a4brun er34gjf0"})
     link_to_offer_unfiltered = soup.find_all("a",attrs={"class":"css-rc5s2u"})
     link_to_offer = ["https://olx.pl/" + link['href'] for link in link_to_offer_unfiltered]
-    
+
     img_urls = []
     for title in titles:
         img_tag = soup.find("img", alt=title.text.strip())
@@ -23,7 +23,6 @@ def scrape_page(page_number,location,transaction,number_of_rooms):
     data = [(title.text.strip(), price.text.strip(), location.text.strip(), link,image_url) \
         for title, price, location, link,image_url in zip(titles, prices, location_of_offer, link_to_offer,img_urls)]
 
-    print(data)
 
     return data
 
@@ -32,7 +31,7 @@ def scrape_page(page_number,location,transaction,number_of_rooms):
 def scrape_multiple_pages(start_page, end_page,location="",price="",keyword="",area="",price_per_sqm="",sorting="",transaction="",number_of_rooms=""):
     offers_data = []
     with ThreadPoolExecutor(max_workers=5) as executor:
-        future_to_page = {executor.submit(scrape_page, page_number,location,transaction,number_of_rooms): page_number for page_number in range(start_page, end_page + 1)}
+        future_to_page = {executor.submit(scrape_page, page_number,location,transaction,number_of_rooms,sorting): page_number for page_number in range(start_page, end_page + 1)}
         for future in as_completed(future_to_page): 
             page_number = future_to_page[future]
             try:
